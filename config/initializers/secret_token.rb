186b3b43b5573785b93784de3e77768651489c70
Thu Quach <thuquach90@gmail.com>
2013-07-25 13:26:20 -0700

    Generate the Student Project app in Rails 3.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentProject::Application.config.secret_token = 'e922d35f5683a2c55958380869e53cae51c8829d7e744f6a46e9ba38b57ed3cdd3520885b4c7e86a2375d17a94c37d414396a41a5afa55b3ccc6e560ae93a398'
